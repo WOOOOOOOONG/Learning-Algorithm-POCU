1b5facb323afe09aa15fb3f0e8f58165d5045f61
WOOOOOOOONG <sterme9@naver.com>
2021-05-18 10:27:03 +0900

    no message

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -7,9 +7,8 @@ public final class MissionControl {
     private MissionControl() {
     }
 
-    // 퀵 정렬 사용
     public static int findMaxAltitudeTime(final int[] altitudes) {
-        for (int i = altitudes.length- 1 ; i > 0; i--) {
+        for (int i = altitudes.length - 1 ; i > 0; i--) {
             if (altitudes[i] > altitudes[i - 1]) {
                 return i;
             }
@@ -17,81 +16,29 @@ public final class MissionControl {
         return -1;
     }
 
-    /* findAltitudeTimes 해법
-    *  1. 퀵정렬하여 배열을 새로 생성 (new int[][] 형식으로 생성, [0][i]에 인덱스, [1][i]에 값)
-    *  2. 이진탐색
-    *  3. 이진 탐색 이후 찾은 값의 왼쪽과 오른쪽값을 살펴 같은 값이 나올 때까지 newArray에 저장
-    *  시간 복잡도 : (log n * log n) + targetAltitude 수
-    * */
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
         ArrayList<Integer> newArray = new ArrayList<>();
+
         for (int i = 0; i < altitudes.length; i++) {
+            if (targetAltitude < altitudes[i]) {
+                break;
+            }
+
+            if (altitudes[i] == targetAltitude) {
+                newArray.add(i);
+            }
+        }
+
+        for (int i = altitudes.length - 1; i >= 0; i--) {
+            if (targetAltitude < altitudes[i]) {
+                break;
+            }
+
             if (altitudes[i] == targetAltitude) {
                 newArray.add(i);
             }
         }
 
         return newArray;
-    }
-
-    // 이차원 배열 : [0][i] = 원래 인덱스, [1][i] = 정렬된 인덱스
-    public static int[][] quickSortRecursive(int[][] newArr, int index, int[] arr, int l, int r) {
-        if (l >= r) {
-            return null;
-        }
-
-
-        newArr[0][index] = r;
-        int pivotPos = partition(arr, l, r);
-        newArr[1][index++] = pivotPos;
-
-        quickSortRecursive(newArr, index, arr, l, pivotPos - 1);
-        quickSortRecursive(newArr, index, arr, pivotPos + 1, r);
-
-        return newArr;
-    }
-
-    public static int partition(int[] arr, int l, int r) {
-        int pivot = arr[r];
-
-        int i = l;
-        for (int j = l; j < r; j++) {
-            if (arr[j] < pivot) {
-                swap(arr, i, j);
-                i++;
-            }
-        }
-        swap(arr, i, r);
-
-        return i;
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    /* 이진탐색 해법
-    *  1. 배열의 가장 중간 값을 인덱스로 설정한다
-    *  2. 찾고자 하는 값이 배열의 중간이면 탐색 완료, 아니라면 찾는 값이 오른쪽/왼쪽에 있는지 찾아 다시 실행시킨다.
-    *  3. 값을 찾을 때까지 반복한다.
-    * */
-    public static int binarySearch(ArrayList<Integer> arr, int l, int mid, int r, int value) {
-        if (l >= r) {
-            return -1;
-        }
-
-        if (arr.get(mid) == value) {
-            return mid;
-        }
-
-        if (arr.get(mid) > value) {
-            return binarySearch(arr, l, (l + r) / 2, mid, value);
-        } else if (arr.get(mid) < value) {
-            return binarySearch(arr, mid, mid + ((r - mid + 1) / 2), r, value);
-        }
-
-        return 1;
     }
 }
--- Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
+++ Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
@@ -9,75 +9,16 @@ public class Program {
     public static void main(String[] args) {
         final int[] altitudes = new int[] { 1, 2, 3, 4, 5, 6, 7, 4, 3, 2 };
 
-        final int maxAltitudeTime = MissionControl.findMaxAltitudeTime(altitudes); // maxAltitudeTime: 6
-        System.out.println(maxAltitudeTime);
-    }
-
-    public static void puff(int n, int a, int b)
-    {
-        if (n <= 0) {
-            return;
+        ArrayList<Integer> bounds = MissionControl.findAltitudeTimes(altitudes, 2); // bounds: [ 1, 9 ]
+        for (int i = 0; i < bounds.size(); i++) {
+            System.out.print(bounds.get(i) + " ");
         }
-
-        System.out.println(String.format("%d %d %d", n, a, b)); // 4 6 4
-
-        puff(n - 3, a, b + n); // 1 6 8
-        puff(n - 1, b, a + n); // 3 4 10 -> 2 10 7 -> 1 7 12 -> 1 10 7
-        puff(n - 2, b, a + n); // 2 4 10 -> 1 10 6
-    }
-    // 스택 프레임 : 현재 리턴되지 않고 가지고 있는 값
-    // 재귀 함수 : 위와 아래에 있는 재귀들도 조건이 맞다면 실행시킴. (재귀의 재귀가 가능하다는 뜻, 본인 재귀함수의 리턴값에도 다시 가능)
-    // 해당 함수의 최대 스택 프레임 : 4 6 4 -> 3 4 10 -> 2 10 7 -> 1 7 12 -> 1 10 7 : 5개
-
-    /*
-    * 1. 정수 n개의 집합 S와 정수 x를 받아온다.
-    * 2. 새로운 S의 부분 집합을 만든다. ^n
-    * 3. 2번의 부분 집합에 들어 있는 정수들을 모두 더한다. -> n
-    * 4. 3번의 값이 x와 일치하는지 비교한다.
-    * 5. 일치하지 않다면 다시 2번 단계로 돌아간다.
-    * */
-
-    public static int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int min = 999999;
-            int minIndex = -1;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    minIndex = j;
-                }
-            }
-
-            int temp = arr[i];
-            arr[i] = min;
-            arr[minIndex] = temp;
+        System.out.println();
+        bounds = MissionControl.findAltitudeTimes(altitudes, 5); // bounds: [ 4 ]
+        for (int i = 0; i < bounds.size(); i++) {
+            System.out.print(bounds.get(i) + " ");
         }
-
-        return arr;
-    }
-
-    // 다음 값을 알맞은 위치에 삽입한다고 하여 삽입 정렬
-    public static int[] insertionSort(int[] arr) {
-        if (arr.length <= 1) {
-            return arr;
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            while (arr[j - 1] > arr[j]) {
-                int temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-
-                j--;
-
-                if (j < 1) {
-                    break;
-                }
-            }
-        }
-
-        return arr;
+        System.out.println();
     }
 
 }
