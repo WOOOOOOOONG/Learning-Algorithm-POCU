206ede563b79ed5adb819f081cf8211ea7abde5b
WOOOOOOOONG <sterme9@naver.com>
2021-05-18 14:18:15 +0900

    no message

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -4,11 +4,14 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public final class MissionControl {
+    int maxIndex;
+
     private MissionControl() {
+        maxIndex = -1;
     }
 
     public static int findMaxAltitudeTime(final int[] altitudes) {
-        for (int i = altitudes.length - 1; i > 0; i--) {
+        /*for (int i = altitudes.length - 1; i > 0; i--) {
             if (altitudes[i] > altitudes[i - 1]) {
                 return i;
             }
@@ -16,9 +19,46 @@ public final class MissionControl {
             if (i == 1) {
                 return 0;
             }
-        }
+        }*/
 
-        return altitudes.length-1;
+        MissionControl maxIndex = new MissionControl();
+        int max = -9999999;
+        quickSearchRecursive(altitudes, altitudes.length / 2, max, maxIndex, 0, altitudes.length - 1);
+        return maxIndex.maxIndex;
+    }
+
+    // 어떤 배열이 들어왔을 때 해당 값의 좌/우측과 비교하여 큰 값인지 비교
+    public static void quickSearchRecursive(final int[] altitudes, int mid, int max, MissionControl maxIndex, int l, int r) {
+        if (mid < 0 || mid >= altitudes.length || l > r || r < l) {
+            return;
+        }
+        int result = partition(altitudes, mid);
+
+        if (result == -1) {
+            quickSearchRecursive(altitudes, mid - ((r - l + 1) / 2), max, maxIndex, l, mid - 1);
+        } else if (result == 0) {
+            /*if (max < altitudes[mid]) {
+                max = altitudes[mid];
+                maxIndex = mid;
+            }*/
+            maxIndex.maxIndex = mid;
+            return;
+        } else if (result == 1) {
+            quickSearchRecursive(altitudes, mid + ((r - mid + 1) / 2), max, maxIndex, mid + 1, r);
+        }
+    }
+    // mid를 절반씩 증가/감소하는 방법
+    // 10까지 인덱스 존재. 5부터 시작. 5 -> 7 -> 9 : max + ((length - max + 1) / 2)
+    // 6,7,7    7 -
+
+    public static int partition(final int[] altitudes, int mid) {
+        if (mid - 1 >= 0 && altitudes[mid] < altitudes[mid - 1]) {
+            return -1;
+        } else if (mid + 1 <= altitudes.length - 1 && altitudes[mid] < altitudes[mid + 1]) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
--- Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
+++ Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
@@ -8,36 +8,42 @@ public class Program {
 
     public static void main(String[] args) {
         {
-            final int[] altitudes = new int[] {};
-
+            final int[] altitudes = new int[]{1, 2, 3, 4, 5, 6, 7, 4, 3, 2};
             final int maxAltitudeTime = MissionControl.findMaxAltitudeTime(altitudes);
-
-            //System.out.println(maxAltitudeTime);
-        }
-
-        {
-            final int[] altitudes = new int[] { 5,4,3,2,1 };
-
-            ArrayList<Integer> bounds = MissionControl.findAltitudeTimes(altitudes, 4);
-
-            for (int i = 0; i < bounds.size(); i++) {
-                System.out.print(bounds.get(i) + " ");
-            }
-            System.out.println();
-            assert (bounds.size() == 2);
-
-            assert (bounds.get(0) == 1);
-            assert (bounds.get(1) == 9);
-
-            bounds = MissionControl.findAltitudeTimes(altitudes, 5);
-            for (int i = 0; i < bounds.size(); i++) {
-                System.out.print(bounds.get(i) + " ");
-            }
-            System.out.println();
-
-            assert (bounds.size() == 1);
-
-            assert (bounds.get(0) == 4);
+            assert (maxAltitudeTime == 6);
+            System.out.println(maxAltitudeTime == 6);
+            final int[] altitudes2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            final int maxAltitudeTime2 = MissionControl.findMaxAltitudeTime(altitudes2);
+            assert (maxAltitudeTime2 == 9);
+            System.out.println(maxAltitudeTime2 == 9);
+            final int[] altitudes3 = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+            final int maxAltitudeTime3 = MissionControl.findMaxAltitudeTime(altitudes3);
+            assert (maxAltitudeTime3 == 0);
+            System.out.println(maxAltitudeTime3 == 0);
+            final int[] altitudes4 = new int[]{3, 5, 9, 8, 7, 5, 5, 4, 3, 1};
+            final int maxAltitudeTime4 = MissionControl.findMaxAltitudeTime(altitudes4);
+            assert (maxAltitudeTime4 == 2);
+            System.out.println(maxAltitudeTime4 == 2);
+            final int[] altitudes5 = new int[]{3, 5, 9};
+            final int maxAltitudeTime5 = MissionControl.findMaxAltitudeTime(altitudes5);
+            assert (maxAltitudeTime5 == 2);
+            System.out.println(maxAltitudeTime5 == 2);
+            final int[] altitudes6 = new int[]{9, 7, 3};
+            final int maxAltitudeTime6 = MissionControl.findMaxAltitudeTime(altitudes6);
+            assert (maxAltitudeTime6 == 0);
+            System.out.println(maxAltitudeTime6 == 0);
+            final int[] altitudes7 = new int[]{3, 5};
+            final int maxAltitudeTime7 = MissionControl.findMaxAltitudeTime(altitudes7);
+            assert (maxAltitudeTime7 == 1);
+            System.out.println(maxAltitudeTime7 == 1);
+            final int[] altitudes8 = new int[]{5, 3};
+            final int maxAltitudeTime8 = MissionControl.findMaxAltitudeTime(altitudes8);
+            assert (maxAltitudeTime8 == 0);
+            System.out.println(maxAltitudeTime8 == 0);
+            final int[] altitudes9 = new int[]{5};
+            final int maxAltitudeTime9 = MissionControl.findMaxAltitudeTime(altitudes9);
+            assert (maxAltitudeTime9 == 0);
+            System.out.println(maxAltitudeTime9 == 0);
         }
     }
 }
