44d0370ac6716252b3dc2e1c678fc5c60a0dadaa
DESKTOP-NLLR8JA\user <sterme92@gmail.com>
2021-06-07 02:25:50 +0900

    no message

--- Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -4,16 +4,27 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class KeyGenerator {
-
-    public static boolean isPrime(final BigInteger number) {
-        if (number.intValue() <= 0) {
+    public static boolean isPrime(final BigInteger n) {
+        if (n.intValue() <= 0) {
             return false;
         }
-        if (isPrime(number)) {
+
+        BigInteger[] alist = {BigInteger.valueOf(2), BigInteger.valueOf(325), BigInteger.valueOf(9375), BigInteger.valueOf(28178), BigInteger.valueOf(450775), BigInteger.valueOf(9780504), BigInteger.valueOf(1795265022)};
+
+        if (n.compareTo(BigInteger.ONE) == 0 || n.compareTo(BigInteger.ONE) == -1)
+            return false;
+        if (n.compareTo(new BigInteger("10000000000")) == 0 || n.compareTo(new BigInteger("10000000000")) == -1) {
+            for (BigInteger i = BigInteger.TWO; i.multiply(i).compareTo(n) == 0 || i.multiply(i).compareTo(n) == -1; i = i.add(BigInteger.ONE)) {
+                if (n.mod(i).compareTo(BigInteger.ZERO) == 0)
+                    return false;
+            }
             return true;
         }
 
-        return false;
+        for (BigInteger a : alist)
+            if (!millerRabin(n, a))
+                return false;
+        return true;
     }
 
     // calculate (x + y) % m; overflow-safe
@@ -65,24 +76,5 @@ public class KeyGenerator {
         }
         BigInteger tmp = powmod(a, d, n);
         return tmp.compareTo(n.subtract(BigInteger.ONE)) == 0 || tmp.compareTo(BigInteger.ONE) == 0;
-    }
-
-    public static boolean isPrime(BigInteger n) {
-        BigInteger[] alist = {BigInteger.valueOf(2), BigInteger.valueOf(325), BigInteger.valueOf(9375), BigInteger.valueOf(28178), BigInteger.valueOf(450775), BigInteger.valueOf(9780504), BigInteger.valueOf(1795265022)};
-
-        if (n.compareTo(BigInteger.ONE) == 0 || n.compareTo(BigInteger.ONE) == -1)
-            return false;
-        if (n.compareTo(new BigInteger("10000000000")) == 0 || n.compareTo(new BigInteger("10000000000")) == -1) {
-            for (BigInteger i = BigInteger.TWO; i.multiply(i).compareTo(n) == 0 || i.multiply(i).compareTo(n) == -1; i = i.add(BigInteger.ONE)) {
-                if (n.mod(i).compareTo(BigInteger.ZERO) == 0)
-                    return false;
-            }
-            return true;
-        }
-
-        for (BigInteger a : alist)
-            if (!millerRabin(n, a))
-                return false;
-        return true;
     }
 }
