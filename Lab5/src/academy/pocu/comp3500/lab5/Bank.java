6e6bf23b285e2522f83c4d5877004e286c44b53f
DESKTOP-NLLR8JA\user <sterme92@gmail.com>
2021-06-06 15:02:52 +0900

    no message

--- Lab5/src/academy/pocu/comp3500/lab5/Bank.java
+++ Lab5/src/academy/pocu/comp3500/lab5/Bank.java
@@ -1,5 +1,11 @@
 package academy.pocu.comp3500.lab5;
 
+import javax.crypto.Cipher;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.LinkedList;
 
 public class Bank {
@@ -30,6 +36,58 @@ public class Bank {
         int receiver = -1;
         boolean bTransferFind = false;
         boolean bReceiverFind = false;
+        byte[] message;
+        int messageLength;
+        int messageIndex = 0;
+
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(amount);
+        byte[] amountToBytes = buffer.array();
+
+        messageLength = from.length + to.length + amountToBytes.length;
+        message = new byte[messageLength];
+
+        System.arraycopy(from, 0, message, 0, from.length);
+        System.arraycopy(to, 0, message, from.length, to.length);
+        System.arraycopy(amountToBytes, 0, message, from.length + to.length, amountToBytes.length);
+
+        /*
+        for (int i = 0; i < from.length; i++) {
+            message[messageIndex++] = from[i];
+        }
+        for (int i = 0; i < to.length; i++) {
+            message[messageIndex++] = to[i];
+        }
+        for (int i = 0; i < amountToBytes.length; i++) {
+            message[messageIndex++] = amountToBytes[i];
+        }
+        for (int i = 0; i < message.length; i++) {
+            System.out.print(message[i]);
+        }
+        System.out.println();
+        */
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(message);
+            StringBuffer hexString = new StringBuffer(); // sha256([from, to, amount])
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            // pubKey 생성
+            PublicKey pubKey = getPublicKey(from);
+            // signature 디코딩
+            String decoding = decrypt(signature, pubKey);
+
+            System.out.println(decoding);
+            System.out.println(hexString);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         // from, to의 인덱스 구해 각각 인출/송금 계산
         for (int i = 0; i < pubKeys.length; i++) {
@@ -70,5 +128,64 @@ public class Bank {
 
             return true;
         }
+    }
+
+    private static String decrypt(byte[] ciphertext, PublicKey publicKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, publicKey);
+
+            byte[] plaintext = cipher.doFinal(ciphertext);
+
+            return new String(plaintext, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static PublicKey getPublicKey(byte[] from) {
+        try {
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(from);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey pubKey = keyFactory.generatePublic(keySpec);
+
+            return pubKey;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static KeyPair getKeyPair() {
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+
+            generator.initialize(2048, new SecureRandom());
+            KeyPair pair = generator.generateKeyPair();
+
+            return pair;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] decodeFromHexString(String hexString) {
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            int firstDigit = Character.digit(hexString.charAt(i), 16);
+            int secondDigit = Character.digit(hexString.charAt(i + 1), 16);
+            bytes[i / 2] = (byte) ((firstDigit << 4) + secondDigit);
+        }
+        return bytes;
+    }
+
+    private static String encodeToHexString(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte oneByte : bytes) {
+            result.append(String.format("%02x", oneByte));
+        }
+        return result.toString();
     }
 }
--- Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -4,11 +4,79 @@ import java.math.BigInteger;
 
 public class KeyGenerator {
     public static boolean isPrime(final BigInteger number) {
-        for (int i = 2; i * i <= number.intValue(); i++) {
-            if (number.intValue() % i == 0)
-                return false;
+        if (isPrime(number.intValue(), 100)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isPrime(long candidate, long accuracy) {
+        long d, s;
+
+        if (candidate == 2)
+            return true;
+        if (candidate < 2)
+            return false;
+
+        // until d is odd
+        for (d = 0, s = 1; (d & 1) == 0; s++)
+            d = (candidate - 1) / fastPow(2, s);
+
+        verification: for (long i = 0; i < accuracy; i++) {
+            // random base in the range [2, n-1]
+            long base = (long) ((Math.random() * (candidate - 3)) + 2);
+
+            long x = fastPow(base, d, candidate);
+
+            if (x == 1 || x == (candidate - 1))
+                continue verification;
+
+            for (long j = 0; j < (s - 1); j++) {
+                x = fastPow(x, 2, candidate);
+                if (x == 1)
+                    return false;
+                if (x == (candidate - 1))
+                    continue verification;
+            }
+
+            return false;
         }
 
         return true;
+    }
+
+    private static long fastPow(long base, long exponent) {
+        int shift = 63; // bit position
+        long result = base; // (1 * 1) * base = base
+
+        // Skip all leading 0 bits and the most significant 1 bit.
+        while (((exponent >> shift--) & 1) == 0)
+            ;
+
+        while (shift >= 0) {
+            result = result * result;
+            if (((exponent >> shift--) & 1) == 1)
+                result = result * base;
+        }
+
+        return result;
+    }
+
+    private static long fastPow(long base, long exponent, long modulo) {
+        int shift = 63; // bit position
+        long result = base; // (1 * 1) * base = base
+
+        // Skip all leading 0 bits and the most significant 1 bit.
+        while (((exponent >> shift--) & 1) == 0)
+            ;
+
+        while (shift >= 0) {
+            result = (result * result) % modulo;
+            if (((exponent >> shift--) & 1) == 1)
+                result = (result * base) % modulo;
+        }
+
+        return result;
     }
 }
