9ba26f4b33f23efabb79a2c9de4dfed3e58b09cf
DESKTOP-NLLR8JA\user <sterme92@gmail.com>
2021-06-07 02:07:15 +0900

    no message

--- Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -4,52 +4,85 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class KeyGenerator {
+
     public static boolean isPrime(final BigInteger number) {
         if (number.intValue() <= 0) {
             return false;
         }
-        if (isProbablePrime(number, 100)) {
+        if (is_prime(number)) {
             return true;
         }
 
         return false;
     }
 
-    public static boolean isProbablePrime(BigInteger n, int k) {
-        if (n.compareTo(new BigInteger("3")) < 0)
-            return true;
-        int s = 0;
-        BigInteger d = n.subtract(BigInteger.ONE); // n-1
-        while (d.mod(BigInteger.TWO).equals(BigInteger.ZERO)) { //?
-            s++;                          //?
-            d = d.divide(BigInteger.TWO);            //?
+    // calculate (x + y) % m; overflow-safe
+    public static BigInteger addmod(BigInteger x, BigInteger y, BigInteger m) {
+        x = x.mod(m);
+        y = y.mod(m);
+
+        if ((x.compareTo(x.subtract(y)) == 1 || x.compareTo(x.subtract(y)) == 0)) {
+            return x.subtract(m.subtract(y));
+        } else {
+            return x.add(y);
         }
-        for (int i = 0; i < k; i++) {    //LOOP: repeat k times
-            BigInteger a = uniformRandom(BigInteger.TWO, n.subtract(BigInteger.ONE)); //?
-            BigInteger x = a.modPow(d, n);  //x = a^d mod n
-            if (x.equals(BigInteger.ONE) || x.equals(n.subtract(BigInteger.ONE))) // if x=1 or x = n-1, then do next LOOP
-                continue;
-            int r = 1;
-            for (; r < s; r++) { // for r = 1..s-1
-                x = x.modPow(BigInteger.TWO, n);  //x = x ^ 2 mod n
-                if (x.equals(BigInteger.ONE))     //if x = 1, return false (composite
-                    return false;
-                if (x.equals(n.subtract(BigInteger.ONE))) //if x= n-1, look at the next a
-                    break;
-            }
-            if (r == s) // None of the steps made x equal n-1.
-                return false; //we've exhausted all of our a values, probably composite
-        }
-        return true; //probably prime
     }
 
-    //this method is just to generate a random int
-    private static BigInteger uniformRandom(BigInteger bottom, BigInteger top) {
-        Random rnd = new Random();
-        BigInteger res;
-        do {
-            res = new BigInteger(top.bitLength(), rnd);
-        } while (res.compareTo(bottom) < 0 || res.compareTo(top) > 0);
-        return res;
+    // calculate (x * y) % m; overlow-safe
+    public static BigInteger mulmod(BigInteger x, BigInteger y, BigInteger m) {
+        x = x.mod(m);
+        y = y.mod(m);
+        BigInteger r = BigInteger.ZERO;
+        while (y.compareTo(BigInteger.ZERO) == 1) {
+            if (y.mod(BigInteger.TWO).compareTo(BigInteger.ONE) == 1)
+                r = addmod(r, x, m);
+            x = addmod(x, x, m);
+            y = y.divide(BigInteger.TWO);
+        }
+        return r;
+    }
+
+    // calculate x^y % m; overflow-safe
+    static BigInteger powmod(BigInteger x, BigInteger y, BigInteger m) {
+        x = x.mod(m);
+        BigInteger r = BigInteger.ONE;
+        while (y.compareTo(BigInteger.ZERO) == 1) {
+            if (y.mod(BigInteger.TWO).compareTo(BigInteger.ONE) == 0)
+                r = mulmod(r, x, m);
+            x = mulmod(x, x, m);
+            y = y.divide(BigInteger.TWO);
+        }
+        return r;
+    }
+
+    // true for probable prime, false for composite
+    public static boolean miller_rabin(BigInteger n, BigInteger a) {
+        BigInteger d = n.subtract(BigInteger.ONE);
+        while (d.mod(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0) {
+            if (powmod(a, d, n).compareTo(n.subtract(BigInteger.ONE)) == 0)
+                return true;
+            d = d.divide(BigInteger.TWO);
+        }
+        BigInteger tmp = powmod(a, d, n);
+        return tmp.compareTo(n.subtract(BigInteger.ONE)) == 0 || tmp.compareTo(BigInteger.ONE) == 0;
+    }
+
+    public static boolean is_prime(BigInteger n) {
+        BigInteger[] alist = {BigInteger.valueOf(2), BigInteger.valueOf(325), BigInteger.valueOf(9375), BigInteger.valueOf(28178), BigInteger.valueOf(450775), BigInteger.valueOf(9780504), BigInteger.valueOf(1795265022)};
+
+        if (n.compareTo(BigInteger.ONE) == 0 || n.compareTo(BigInteger.ONE) == -1)
+            return false;
+        if (n.compareTo(new BigInteger("10000000000")) == 0 || n.compareTo(new BigInteger("10000000000")) == -1) {
+            for (BigInteger i = BigInteger.TWO; i.multiply(i).compareTo(n) == 0 || i.multiply(i).compareTo(n) == -1; i = i.add(BigInteger.ONE)) {
+                if (n.mod(i).compareTo(BigInteger.ZERO) == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        for (BigInteger a : alist)
+            if (!miller_rabin(n, a))
+                return false;
+        return true;
     }
 }
