f7fbe36d73d756c3fda6d8b841dcba3c60490587
WOOOOOOOONG <sterme9@naver.com>
2021-07-12 02:20:18 +0900

    no message

---
+++ Lab7/src/academy/pocu/comp3500/lab7/TicTacToe/Player.java
@@ -0,0 +1,6 @@
+package academy.pocu.comp3500.lab7.TicTacToe;
+
+public enum Player {
+    X,
+    O
+}
--- Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -4,78 +4,57 @@ import academy.pocu.comp3500.lab8.maze.Point;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public final class MazeSolver {
-    public static List<Point> findPath(final char[][] maze, final Point start) {
-        List<Point> answer = new ArrayList<>();
-        Point point = new Point(start.getY(), start.getX());
-        int direction = -1; // 0,1,2,3 : 상하좌우
+    private static Stack<Point> stack = new Stack<>();
 
-        answer.add(new Point(point.getY(), point.getX()));
-        if (maze[point.getX()][point.getY()] == 'E') {
-            return answer;
-        }
-        maze[point.getX()][point.getY()] = 'p';
+    public static List<Point> findPath(final char[][] maze, final Point start) {
+        Point cur = start;
+        stack.push(new Point(cur.getY(), cur.getX()));
 
         while (true) {
-            System.out.println(point.getX() + ", " + point.getY());
-
-            if (point.getX() > 0 && (maze[point.getX() - 1][point.getY()] == ' ' || maze[point.getX() - 1][point.getY()] == 'E')) {
-                //System.out.println("hi1");
-                direction = -1;
-                point = new Point(point.getX() - 1, point.getY());
-                answer.add(new Point(point.getY(), point.getX()));
-                if (maze[point.getX()][point.getY()] == 'E') {
-                    return answer;
-                }
-                maze[point.getX()][point.getY()] = 'p';
-            } else if (point.getX() < maze.length && (maze[point.getX() + 1][point.getY()] == ' ' || maze[point.getX() + 1][point.getY()] == 'E')) {
-                //System.out.println("hi2");
-                direction = -1;
-                point = new Point(point.getX() + 1, point.getY());
-                answer.add(new Point(point.getY(), point.getX()));
-                if (maze[point.getX()][point.getY()] == 'E') {
-                    return answer;
-                }
-                maze[point.getX()][point.getY()] = 'p';
-            } else if (point.getY() > 0 && (maze[point.getX()][point.getY() - 1] == ' ' || maze[point.getX()][point.getY() - 1] == 'E')) {
-                //System.out.println("hi3");
-                direction = -1;
-                point = new Point(point.getX(), point.getY() - 1);
-                answer.add(new Point(point.getY(), point.getX()));
-                if (maze[point.getX()][point.getY()] == 'E') {
-                    return answer;
-                }
-                maze[point.getX()][point.getY()] = 'p';
-            } else if (point.getY() > 0 && (maze[point.getX()][point.getY() + 1] == ' ' || maze[point.getX()][point.getY() + 1] == 'E')) {
-                //System.out.println("hi4");
-                direction = -1;
-                point = new Point(point.getX(), point.getY() + 1);
-                answer.add(new Point(point.getY(), point.getX()));
-                if (maze[point.getX()][point.getY()] == 'E') {
-                    return answer;
-                }
-                maze[point.getX()][point.getY()] = 'p';
-            } else if (direction != 1 && point.getX() > 0 && maze[point.getX() - 1][point.getY()] == 'p') {
-                //System.out.println("hi5");
-                direction = 0;
-                point = new Point(point.getX() - 1, point.getY());
-            } else if (direction != 0 && point.getX() < maze.length && maze[point.getX() + 1][point.getY()] == 'p') {
-                //System.out.println("hi6");
-                direction = 1;
-                point = new Point(point.getX() + 1, point.getY());
-            } else if (direction != 3 && point.getY() > 0 && maze[point.getX()][point.getY() - 1] == 'p') {
-                //System.out.println("hi7");
-                direction = 2;
-                point = new Point(point.getX(), point.getY() - 1);
-            } else if (direction != 2 && point.getY() > 0 && maze[point.getX()][point.getY() + 1] == 'p') {
-                //System.out.println("hi8");
-                direction = 3;
-                point = new Point(point.getX(), point.getY() + 1);
-            } else {
-                //System.out.println("hi9");
-                return new ArrayList<Point>();
+            if (maze[cur.getX()][cur.getY()] == 'E') {
+                stack.push(new Point(cur.getY(), cur.getX()));
+                stack.remove(0);
+                return stack;
             }
+            maze[cur.getX()][cur.getY()] = 'p';
+            printMaze(maze);
+
+             if ((maze[cur.getX() - 1][cur.getY()] == ' ' || maze[cur.getX() - 1][cur.getY()] == 'E') && (!(stack.peek().getX() == cur.getX() - 1 && stack.peek().getY() == cur.getY()))) {
+                 stack.push(new Point(cur.getY(), cur.getX()));
+                 cur = new Point(cur.getX() - 1, cur.getY());
+            } else if ((maze[cur.getX() + 1][cur.getY()] == ' ' || maze[cur.getX() + 1][cur.getY()] == 'E') && (!(stack.peek().getX() == cur.getX() + 1 && stack.peek().getY() == cur.getY()))) {
+                 stack.push(new Point(cur.getY(), cur.getX()));
+                 cur = new Point(cur.getX() + 1, cur.getY());
+            } else if ((maze[cur.getX()][cur.getY() - 1] == ' ' || maze[cur.getX()][cur.getY() - 1] == 'E') && (!(stack.peek().getX() == cur.getX() && stack.peek().getY() == cur.getY() - 1))) {
+                 stack.push(new Point(cur.getY(), cur.getX()));
+                 cur = new Point(cur.getX(), cur.getY() - 1);
+            } else if ((maze[cur.getX()][cur.getY() + 1] == ' '  || maze[cur.getX()][cur.getY() + 1] == 'E') && (!(stack.peek().getX() == cur.getX() && stack.peek().getY() == cur.getY() + 1))) {
+                 stack.push(new Point(cur.getY(), cur.getX()));
+                 cur = new Point(cur.getX(), cur.getY() + 1);
+            }  else {
+                 cur = new Point(stack.peek().getY(), stack.peek().getX());
+                 stack.pop();
+             }
+
+             if (stack.isEmpty()) {
+                 break;
+             }
         }
+
+        //findPath(maze, new Point(start.getX() - 1, start.getY()));
+        return stack;
+    }
+
+    public static void printMaze(char[][] maze) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
